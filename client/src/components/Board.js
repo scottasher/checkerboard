3a0273819e795183ace4811e51544b7a2164174a
scottasher <34749194+scottasher@users.noreply.github.com>
2020-03-19 20:15:25 -0700

    Add files via upload

---
+++ client/src/components/Board.js
@@ -0,0 +1,103 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div>
+            <div className="row"> 
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col"></div> 
+            </div>
+            
+            <div className="row">
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div> 
+            </div>
+            
+            <div className="row">
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col"></div> 
+            </div>
+            
+            <div className="row">
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div> 
+            </div>
+            
+            <div className="row">
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col"></div> 
+            </div>
+            
+            <div className="row">
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div> 
+            </div>
+            
+            <div className="row">
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col black"></div>
+                <div style={props.style} className="square col"></div>
+                <div style={props.style} className="square col"></div> 
+            </div>
+            
+            <div class="row">
+                <div style={props.style} class="square col"></div>
+                <div style={props.style} class="square col black"></div>
+                <div style={props.style} class="square col"></div>
+                <div style={props.style} class="square col black"></div>
+                <div style={props.style} class="square col"></div>
+                <div style={props.style} class="square col black"></div>
+                <div style={props.style} class="square col"></div>
+                <div style={props.style} class="square col black"></div>
+                <div style={props.style} class="square col"></div> 
+            </div>
+        </div>
+    )
+}
---
+++ client/src/components/Checkers.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <>
+            <div class="checker white-checker"> </div>
+            <div class="checker white-checker"> </div>
+            <div class="checker white-checker"> </div>
+            <div class="checker white-checker"> </div>
+            <div class="checker white-checker"> </div>
+            <div class="checker white-checker"> </div>
+            <div class="checker white-checker"> </div>
+            <div class="checker white-checker"> </div>	
+            <div class="checker white-checker"> </div>
+            <div class="checker white-checker"> </div>
+            <div class="checker white-checker"> </div>
+            <div class="checker white-checker"> </div>	
+
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+            <div class="checker black-checker"> </div>
+        </>
+    )
+}
